7dd35fe6fca48f763c038021337cbf0785a3c016
Matthew Callen <mattdcallen2004@gmail.com>
2021-10-07 22:22:13 -0400

    git branching

--- SoccerStats/SoccerDB/views.py
+++ SoccerStats/SoccerDB/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. Testing github connection.")
+    return HttpResponse("Hello, world. Testing github branching.")

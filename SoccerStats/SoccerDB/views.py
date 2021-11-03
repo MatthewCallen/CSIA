e83797438f9b49ed99b900d973422c24c61b792e
Matthew Callen <mattdcallen2004@gmail.com>
2021-11-02 20:42:47 -0400

    testingGit

--- SoccerStats/SoccerDB/views.py
+++ SoccerStats/SoccerDB/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. Testing github branching.")
+    return HttpResponse("Hello, world. Testing github changes.")
